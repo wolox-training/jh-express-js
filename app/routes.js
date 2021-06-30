df9cd4c8dab1a9dbeb305d0fac6fda51e008fc95
Felipe Hernández <jose.hernandez@wolox.co>
2021-06-30 13:52:35 -0500

    Removed annotated lines in example routes

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,4 @@ const { registerUser } = require('./middlewares/users');
 exports.init = app => {
   app.get('/health', healthCheck);
   app.post('/users', [registerUser], userController.createUser);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
 };
